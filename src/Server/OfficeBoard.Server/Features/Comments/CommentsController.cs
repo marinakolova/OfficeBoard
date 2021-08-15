33c2bb909288bd5d774c540d6a384933b15594fe
marinakolova <marina.kolova.359@gmail.com>
2021-08-15 15:20:59 +0300

    Server REST Post Request - Create Comment

--- src/Server/OfficeBoard.Server/Features/Comments/CommentsController.cs
+++ src/Server/OfficeBoard.Server/Features/Comments/CommentsController.cs
@@ -1,9 +1,37 @@
 ﻿namespace OfficeBoard.Server.Features.Comments
 {
+    using System.Threading.Tasks;
+
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using OfficeBoard.Server.Features.Comments.Models;
+    using OfficeBoard.Server.Infrastructure.Services;
 
     [Authorize]
     public class CommentsController : ApiController
     {
+        private readonly ICommentService commentService;
+        private readonly ICurrentUserService currentUserService;
+
+        public CommentsController(
+            ICommentService commentsService,
+            ICurrentUserService currentUserService)
+        {
+            this.commentService = commentsService;
+            this.currentUserService = currentUserService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CommentCreateRequestModel model)
+        {
+            var userId = this.currentUserService.GetId();
+
+            var commentId = await this.commentService.Create(
+                model.Content,
+                model.TaskId,
+                userId);
+
+            return this.Created(nameof(this.Create), commentId);
+        }
     }
 }
