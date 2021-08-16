78298cf8656492bae442ed48cb087f429428f0a4
marinakolova <marina.kolova.359@gmail.com>
2021-08-16 16:14:40 +0300

    Server Change Seeders

--- src/Server/OfficeBoard.Server/Data/Seeding/CustomSeeders/CommentsSeeder.cs
+++ src/Server/OfficeBoard.Server/Data/Seeding/CustomSeeders/CommentsSeeder.cs
@@ -19,26 +19,50 @@
                 new Comment
                 {
                     Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    TaskId = dbContext.Tasks.FirstOrDefault().Id,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    TaskId = dbContext.Tasks.OrderBy(x => x.Id).FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
                 },
                 new Comment
                 {
                     Content = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                    TaskId = dbContext.Tasks.FirstOrDefault().Id,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    TaskId = dbContext.Tasks.OrderBy(x => x.Id).FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
                 },
                 new Comment
                 {
                     Content = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-                    TaskId = dbContext.Tasks.FirstOrDefault().Id,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    TaskId = dbContext.Tasks.OrderBy(x => x.Id).FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
                 },
                 new Comment
                 {
                     Content = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    TaskId = dbContext.Tasks.FirstOrDefault().Id,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    TaskId = dbContext.Tasks.OrderBy(x => x.Id).FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
+                },
+                new Comment
+                {
+                    Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    TaskId = dbContext.Tasks.OrderBy(x => x.Id).LastOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
+                },
+                new Comment
+                {
+                    Content = "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                    TaskId = dbContext.Tasks.OrderBy(x => x.Id).LastOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
+                },
+                new Comment
+                {
+                    Content = "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                    TaskId = dbContext.Tasks.OrderBy(x => x.Id).LastOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
+                },
+                new Comment
+                {
+                    Content = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    TaskId = dbContext.Tasks.OrderBy(x => x.Id).LastOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
                 },
             };
 
--- src/Server/OfficeBoard.Server/Data/Seeding/CustomSeeders/TasksSeeder.cs
+++ src/Server/OfficeBoard.Server/Data/Seeding/CustomSeeders/TasksSeeder.cs
@@ -21,70 +21,70 @@
                     Title = "Lorem ipsum dolor sit amet",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)0,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "consectetur adipiscing elit",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)0,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "sed do eiusmod tempor incididunt",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)0,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "ut labore et dolore magna aliqua",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)1,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "Ut enim ad minim veniam",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)1,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "quis nostrud exercitation ullamco laboris",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)2,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "nisi ut aliquip ex ea commodo consequat",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)2,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "Duis aute irure dolor in reprehenderit",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)2,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).FirstOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "in voluptate velit esse cillum",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)2,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
                 },
                 new Task
                 {
                     Title = "dolore eu fugiat nulla pariatur",
                     Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     Status = (TaskStatus)2,
-                    UserId = dbContext.Users.FirstOrDefault().Id,
+                    UserId = dbContext.Users.OrderBy(x => x.Id).LastOrDefault().Id,
                 },
             };
 
